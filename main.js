1c22707c3f261e586be2848ca8556d629a57a131
Valen <valentinmaidanaaa@gmail.com>
2024-05-04 22:48:27 -0300

    BorreComentarios

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// const productos = [
-//     {nombre: "Iphone11", precio: 1500},
-//     {nombre: "Iphone12", precio: 2000},
-//     {nombre: "Iphone13", precio: 2500},
-//     {nombre: "Iphone14", precio: 3000}
-// ];
-
-
-// let productoElegido = prompt("Ingrese el iphone que quiera comprar");
-
-// console.log( producto.find((producto) => producto.nombre === productoElegido))
 
 alert("Bienvenido a Iphone.CdelU\n\nTenemos todos los modelos de iphone comenzando desde el Iphone 8 en adelante.")
 
@@ -23,11 +12,6 @@ const productos = [
     {nombre: "Iphone 15"}
 ];
 
-// const mostrarProductos = () => {
-//     let listaDeProductos = "Lista de productos:\n";
-//     productos.forEach((producto) => producto.nombre === producto)
-// }
-// alert(listaDeProductos);
 
 const mostrarProductos = () => {
     let listaProductos = "Lista de productos:\n\n";
@@ -51,18 +35,7 @@ for (let i = 0; i < productos.length; i++) {
     }
 }
 
-// Si el producto no se encontró en la lista
 if (!productoEncontrado) {
     alert("El producto que deseas no está disponible");
 }
 
-
-
-// return prompt("Ingrese el Iphone que desea comprar.")
-// let iphoneElegido 
-
-// if (iphoneElegido = productos.nombre) {
-//     alert(`Seleccionaste el ${producto}`)
-// } else {
-//     alert("El producto que deseas no esta disponible")
-// }
